4aa31bebb11aa6577a2049e4619e824a4eb622a1
deepashrirajapandian-dot <deepashrirajapandian@gmail.com>
2026-07-20 09:52:13 +0530

    Create levelOrder.java
    Input
    root =
    [3,9,20,null,null,15,7]
    Output
    [[3],[9,20],[15,7]]
    Expected
    [[3],[9,20],[15,7]]
    test case passed

---
+++ levelOrder.java
@@ -0,0 +1,80 @@
+/* Binary Tree Level Order Traversal
+
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-1000 <= Node.val <= 1000*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class levelOrder{
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null)return res;
+        Queue<TreeNode> q=new LinkedList();
+        q.offer(root);
+        while(!q.isEmpty()){
+            List<Integer>curr=new ArrayList<>();
+            int size=q.size();
+
+            for(int i=0;i<size;i++){
+                TreeNode currNode=q.poll();
+                if(currNode.left!=null){
+                    q.offer(currNode.left);
+                
+                }
+                if(currNode.right!=null){
+                    q.offer(currNode.right);
+                }
+                    curr.add(currNode.val);
+                }
+                res.add(curr);
+
+            }
+        
+        return res;
+    }
+}
+
+/*
+test case
+Input
+root =
+[3,9,20,null,null,15,7]
+Output
+[[3],[9,20],[15,7]]
+Expected
+[[3],[9,20],[15,7]]
+*/
+        
